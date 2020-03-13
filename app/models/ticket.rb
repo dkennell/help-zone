a213a9307eca75e9c102de7432c5c9febac83b6a
David Kennell <davidkennell0@gmail.com>
2020-03-13 14:40:08 -0500

    Make email, title, and body required for tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,7 @@
 class Ticket < ApplicationRecord
+  validates :email, presence: true
+  validates :title, presence: true
+  validates :body, presence: true
+
   belongs_to :help_desk
 end
--- test/models/ticket_test.rb
+++ test/models/ticket_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class TicketTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @help_desk = help_desks(:one)
+    @ticket = Ticket.new(email: "yo@hey.com", title: "yay", body: "woot")
+    @help_desk.tickets << @ticket
+  end
+
+  test "ticket can be created" do
+    assert @ticket.valid?
+  end
+
+  test "email is required" do
+    @ticket.email = nil
+    assert @ticket.invalid?
+  end
+
+  test "title is required" do
+    @ticket.title = nil
+    assert @ticket.invalid?
+  end
+
+  test "body is required" do
+    @ticket.body = nil
+    assert @ticket.invalid?
+  end
 end
