745baf5cca53d06d7c6f6bd587cab39540345839
David Kennell <davidkennell0@gmail.com>
2020-03-25 17:55:21 -0500

    Add configuration for mailers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,21 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Correction: _do_ care.
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
+    authentication: "plain"
+  }
+
 
   config.action_mailer.perform_caching = false
 
