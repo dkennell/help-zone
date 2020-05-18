02e0339a81f43ffd44b5f89fb954b6d8c42012bc
David Kennell <davidkennell0@gmail.com>
2020-05-17 23:18:59 -0400

    Rename main method for ticket stimulus controller

--- app/javascript/packs/controllers/ticket_controller.js
+++ app/javascript/packs/controllers/ticket_controller.js
@@ -2,7 +2,7 @@ import { Controller } from 'stimulus'
 
 export default class extends Controller {
   static targets = [ "comments", "email", "body", "ticketId"]
-  greet(event) {
+  addComment(event) {
     event.preventDefault();
     const commentsList = this.commentsTarget
     buildCommentDOMElement(
