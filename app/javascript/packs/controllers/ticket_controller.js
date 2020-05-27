af487a2dc59769028aab2853d05087e450762a73
David Kennell <davidkennell0@gmail.com>
2020-05-27 09:20:31 -0400

    Prettier

--- app/javascript/packs/controllers/ticket_controller.js
+++ app/javascript/packs/controllers/ticket_controller.js
@@ -1,57 +1,55 @@
-import { Controller } from 'stimulus'
-import moment from 'moment';
+import { Controller } from "stimulus";
+import moment from "moment";
 
 export default class extends Controller {
-  static targets = [ "comments", "email", "body", "ticketId"]
+  static targets = ["comments", "email", "body", "ticketId"];
   addComment(event) {
     event.preventDefault();
 
-    const email = this.emailTarget.value
-    const commentBody = this.bodyTarget.value
-    const ticketId = this.ticketIdTarget.value
+    const email = this.emailTarget.value;
+    const commentBody = this.bodyTarget.value;
+    const ticketId = this.ticketIdTarget.value;
 
     const comment = buildCommentDOMElement(email, commentBody);
     persistCommentObjectToBackend(email, commentBody, ticketId);
 
-    this.commentsTarget.append(comment)
-    window.scrollTo(0,document.body.scrollHeight);
+    this.commentsTarget.append(comment);
+    window.scrollTo(0, document.body.scrollHeight);
   }
 }
 
 function buildCommentDOMElement(email, commentBody) {
-  const element = document.createElement('div');
-  element.classList.add('card', 'w-75', 'mx-auto', 'p-3', 'text-left', 'mb-4')
+  const element = document.createElement("div");
+  element.classList.add("card", "w-75", "mx-auto", "p-3", "text-left", "mb-4");
 
-  const authorNameElement = document.createElement('strong');
-  const dateElement = document.createElement('p');
-  const commentBodyElement = document.createElement('p');
+  const authorNameElement = document.createElement("strong");
+  const dateElement = document.createElement("p");
+  const commentBodyElement = document.createElement("p");
   authorNameElement.innerHTML = email;
-  dateElement.innerHTML = moment().format('h:mma, MMMM D YYYY');
-  commentBodyElement.innerHTML = commentBody
+  dateElement.innerHTML = moment().format("h:mma, MMMM D YYYY");
+  commentBodyElement.innerHTML = commentBody;
 
   element.appendChild(authorNameElement);
   element.appendChild(dateElement);
   element.appendChild(commentBodyElement);
 
-  return element
+  return element;
 }
 
 function persistCommentObjectToBackend(email, body, ticketId) {
-  const authToken = document.querySelector('#authenticity_token').dataset.value;
+  const authToken = document.querySelector("#authenticity_token").dataset.value;
   fetch(`/comments`, {
-    method: 'POST',
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "X-CSRF-Token": authToken
+      "X-CSRF-Token": authToken,
     },
-    body: JSON.stringify(
-      {
-        "comment": {
-          "author": email,
-          "body": body,
-          "ticket_id": ticketId
-        }
-      }      
-    ) 
+    body: JSON.stringify({
+      comment: {
+        author: email,
+        body: body,
+        ticket_id: ticketId,
+      },
+    }),
   });
 }
