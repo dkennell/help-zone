6c164d4aad36c7833f082b38967e2c910d996b4c
David Kennell <davidkennell0@gmail.com>
2020-06-22 10:58:26 -0500

    Mailbox debugging

--- app/mailboxes/application_mailbox.rb
+++ app/mailboxes/application_mailbox.rb
@@ -1,4 +1,11 @@
 class ApplicationMailbox < ActionMailbox::Base
   # routing /something/i => :somewhere
+  puts '=========================='
+  puts '=========================='
+  puts '=========================='
+  puts 'IN APPLICATION MAILBOX ROUTER'
+  puts '=========================='
+  puts '=========================='
+  puts '=========================='
   routing :all => :comments
 end
