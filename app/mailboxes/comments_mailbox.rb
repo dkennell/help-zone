e03e8554d5f02092cd63dbaff192a69794d9f3f7
David Kennell <davidkennell0@gmail.com>
2020-06-24 09:46:47 -0500

    Generate comment upon receipt of email containing a ticket_id.

--- app/mailboxes/comments_mailbox.rb
+++ app/mailboxes/comments_mailbox.rb
@@ -1,6 +1,26 @@
 class CommentsMailbox < ApplicationMailbox
   def process
-    # Temporary comment creation flow; will change once this is tested in prod.
-    Comment.create(body: mail.decoded, ticket_id: Ticket.first.id)
+    Comment.create(body: mail.decoded, ticket_id: get_ticket_id(body: mail.decoded))
   end
+
+  def get_ticket_id(body: "")
+    start_index = body.index('<start_id>') + 10
+    end_index = body.index('<end_id>') - 1
+    ticket_id_string = body[start_index..end_index]
+    return ticket_id_string.to_i
+  end
+
+  # Incoming emails are expected to adhere to a certain format such that comments can
+  # reliably be generated from them. Example follows.
+
+  # Example Email
+  # ---------------
+
+  # Hello I would like assistance with such and such please.
+  
+  # ======================================
+  # Do not alter content below this line
+  # ======================================
+  
+  # Ticket ID: <start_id>20<end_id>
 end
