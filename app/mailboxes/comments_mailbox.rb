112a9b0629351b9b5a6b9cf78db7a85f4fcc9c84
David Kennell <davidkennell0@gmail.com>
2020-06-24 16:26:01 -0500

    Correctly format email-generated ticket comment

--- app/mailboxes/comments_mailbox.rb
+++ app/mailboxes/comments_mailbox.rb
@@ -1,6 +1,12 @@
 class CommentsMailbox < ApplicationMailbox
   def process
-    Comment.create(body: mail.decoded, ticket_id: get_ticket_id(body: mail.decoded))
+    ticket_id = get_ticket_id(body: mail.decoded)
+    comment_body = comment_body_from_email(email_body: mail.decoded)
+    Comment.create(
+      body: comment_body,
+      ticket_id: ticket_id,
+      author: mail.from[0]
+    )
   end
 
   def get_ticket_id(body: "")
@@ -8,6 +14,11 @@ class CommentsMailbox < ApplicationMailbox
     end_index = body.index('<end_id>') - 1
     ticket_id_string = body[start_index..end_index]
     return ticket_id_string.to_i
+  end
+
+  def comment_body_from_email(email_body: '')
+    divider_index = email_body.index('======================================')
+    return email_body[0..divider_index - 1]
   end
 
   # Incoming emails are expected to adhere to a certain format such that comments can
