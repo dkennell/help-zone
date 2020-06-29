1b932d28879c32ceb759296583cbbf0b4350e845
David Kennell <davidkennell0@gmail.com>
2020-06-29 17:44:58 -0500

    Use EmailReplyParser on incoming emails to get sanitized email body

--- app/mailboxes/comments_mailbox.rb
+++ app/mailboxes/comments_mailbox.rb
@@ -1,7 +1,7 @@
 class CommentsMailbox < ApplicationMailbox
   def process
-    ticket_id = get_ticket_id(body: mail.decoded)
-    comment_body = comment_body_from_email(email_body: mail.decoded)
+    ticket_id = get_ticket_id(body: mail.text_part.decoded)
+    comment_body = EmailReplyParser.parse_reply(mail.text_part.decoded)
     Comment.create(
       body: comment_body,
       ticket_id: ticket_id,
@@ -16,13 +16,8 @@ class CommentsMailbox < ApplicationMailbox
     return ticket_id_string.to_i
   end
 
-  def comment_body_from_email(email_body: '')
-    divider_index = email_body.index('======================================')
-    return email_body[0..divider_index - 1]
-  end
-
-  # Incoming emails are expected to adhere to a certain format such that comments can
-  # reliably be generated from them. Example follows.
+  # Incoming emails are expected to contain a ticket ID
+  # between <start_id> and <end_id> tags. Example follows.
 
   # Example Email
   # ---------------
