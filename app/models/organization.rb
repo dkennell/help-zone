c73c8bc3099970a039d37001dfb9fa65440e2b93
David Kennell <davidkennell0@gmail.com>
2020-07-02 21:53:54 -0500

    Fix ticket count on organization index page

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,7 @@
 class Organization < ApplicationRecord
   has_and_belongs_to_many :users
   has_many :help_desks, dependent: :destroy
+  has_many :tickets, through: :help_desks
 
   validates :name, presence: true
 
